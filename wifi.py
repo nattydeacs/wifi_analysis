cc311f25040184de1a7caf863ef550e4595be2c2
nattydeacs <natdeacon@gmail.com>
2022-07-28 11:24:26 -0400

    chart update

--- wifi.py
+++ wifi.py
@@ -10,26 +10,52 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def util_func(speed, E = 2.72, L = 1, k = 0.02, x0 = 175):
+plt.rcParams['figure.dpi'] = 300
+plt.rcParams['savefig.dpi'] = 300
+
+
+def util_func(speed, E = 2.72, L = 1, k = 0.02, x0 = 250):
     return(L/(1+E**(-k*(speed -x0))))
 
 speeds = np.arange(1001)
 utils = util_func(speeds)
 
-sns.scatterplot(data =df, x= 'mbps', y = 'utils')
-
 df = pd.DataFrame({'mbps': speeds, 'utils': utils})
-#df['mbps'] = df['mbps'].astype(str)
 
+#############################################
+#Utils Plot
+#############################################
 
-plt.scatter(df['mbps'], df['utils'])
+NDColors = ["#B0C0BF", "#332A21", "#64A1B4",
+              "#AE8988", "#C36733", "#DD7764", 
+              "#602A10"]
+
+fig = sns.lineplot(data =df, x= 'mbps', y = 'utils', color='#64A1B4', linewidth = 2.5)
+sns.set_style("white")
+sns.despine()
+plt.xlabel("Internet Speed (mbps)")
+plt.ylabel("Customer's Utility (0-1 scale)")
+plt.title("Faster Internet speeds increase utility rapidly up to ~250mbps but plateau around 350mbps")
+
+#############################################
+#Best Internet Plot
+#############################################
 
 prices = pd.read_csv('wifiPrices.csv')
-
-
 plotdf = pd.merge(prices, df, left_on= 'mbps', right_on = 'mbps', how = 'left')
 
-ax = sns.scatterplot(data=plotdf, x="price", y="utils", hue = 'company')
-sns.regplot(data=plotdf, x="price", y="utils", scatter=False, ax = ax, ci = None)
 
-sns.regplot
+fig2 = sns.scatterplot(data=plotdf, x="price", y="utils", hue = 'company', 
+                       palette=(['#6460AC', '#ed1c24', '#46b555']))
+sns.regplot(data=plotdf, x="price", y="utils", scatter=False, ax = fig2, ci = None, 
+            color= 'black', line_kws={'linewidth':.6})
+plt.xlabel("Price/month ($USD)")
+plt.ylabel("Customer's Utility (0-1 scale)")
+plt.title("The Astound 600mbps plan is the best deal")
+for i in range(plotdf.shape[0]):
+ plt.text(x=plotdf.price[i]-1.5,y=plotdf.utils[i]-.05,s=str(plotdf.mbps[i])+'mbps', 
+          fontdict=dict(color='black',size=5.8))
+ 
+
+
+str(plotdf.mbps[1])+'mbps'
